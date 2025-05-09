a15c3dd52bbd72dd190679e6f326bf695d9c441e
SerkanOkut <serkan.okt56@gmail.com>
2025-05-09 14:49:10 +0300

    Initial commit

---
+++ src/test/java/com/twitterapp/TwitterapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.twitterapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
